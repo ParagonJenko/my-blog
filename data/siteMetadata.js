70fb31e4ed33cfb665d4c233d266c63865304e93
Alex Jenkinson <alexjenkinson@ideasquared.co.uk>
2023-10-23 21:14:51 +0100

    Updating about and logo

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -6,8 +6,8 @@ const siteMetadata = {
   description: 'A personal blog for Alex Jenkinson',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteUrl: 'https://alexjenkinson.com',
+  siteRepo: 'https://github.com/ParagonJenko/my-blog',
   siteLogo: '/static/images/logo.png',
   socialBanner: '/static/images/twitter-card.png',
   mastodon: 'https://mastodon.social/@mastodonuser',
