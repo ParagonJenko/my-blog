a8e2838d14dd6411b84c0f7df589c18f1282ab47
Alex Jenkinson <alexjenkinson@ideasquared.co.uk>
2023-10-24 14:06:20 +0100

    Updated projects

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -8,8 +8,32 @@ const projectsData = [
   {
     title: 'Tarkov Community',
     description: `This project is the creation of a central Escape From Tarkov website to showcase all the important links.`,
-    imgSrc: '/static/images/tarkovcommunity-website.png',
+    imgSrc: '/static/images/tarkov-community-website.png',
     href: 'https://tarkovcommunity.com',
+  },
+  {
+    title: 'Paragon Jenko',
+    description: `Dive into thrilling gameplay on my YouTube channel dedicated to all things gaming.`,
+    imgSrc: '/static/images/paragon-jenko.jpg',
+    href: 'https://youtube.com/paragonjenko',
+  },
+  {
+    title: 'Edited for ThatFriendlyGuy',
+    description: `See my experience being THE Tarkov Pacifist's YouTube video editor, capturing the essence of their friendly and entertaining Escape from Tarkov content.`,
+    imgSrc: '/static/images/double-dstroyer.png',
+    href: '/blog/my-whirlwind-journey-in-the-world-of-content-creation',
+  },
+  {
+    title: 'Edited for Double Dstroyer',
+    description: `A montage of high-energy gaming moments and highlights in a fast-paced montage from an awesome streamer`,
+    imgSrc: '/static/images/double-dstroyer.png',
+    href: '/blog/my-whirlwind-journey-in-the-world-of-content-creation',
+  },
+  {
+    title: 'Edited for British Ubba',
+    description: `Learn the art of maximizing in-game earnings with a comprehensive guide by a dedicated male streamer - alongisde other content created!`,
+    imgSrc: '/static/images/british-ubba.png',
+    href: '/blog/my-whirlwind-journey-in-the-world-of-content-creation',
   },
 ]
 
