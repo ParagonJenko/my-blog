75342cc21d8b1599962e0716367736661eb34b16
Alex Jenkinson <alexjenkinson@ideasquared.co.uk>
2023-10-24 20:21:07 +0100

    Readding analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -87,6 +87,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             </div>
           </SectionContainer>
         </ThemeProviders>
+        <Analytics />
       </body>
     </html>
   )
